a0222bbab465d596ca070ce51f09e94e52cdd63b
trantung <melodyinmyheart91@gmail.com>
2018-04-27 13:15:24 +0700

    init project

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(player.ToString());
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
